a491a7d8261a082c967590017e1a5699e0964128
Michal Trela <mmichal.trela@gmail.com>
2024-04-24 20:04:01 +0200

    Move script

--- thermal_detection/thermal_detection.py
+++ thermal_detection/thermal_detection.py
@@ -1,38 +1,19 @@
-import tensorflow as tf
-import matplotlib.pyplot as plt
+from keras import layers, models
 
-from keras import datasets, layers, models, Input
-
-inputs = Input(shape=(32, 32, 3))
-x = layers.Conv2D(32, (3, 3), activation="relu")(inputs)
-x = layers.MaxPooling2D((2, 2))(x)
-x = layers.Conv2D(64, (3, 3), activation="relu")(x)
-x = layers.MaxPooling2D((2, 2))(x)
-x = layers.Conv2D(64, (3, 3), activation="relu")(x)
-x = layers.MaxPooling2D((2, 2))(x)
-x = layers.Flatten()(x)
-x = layers.Dense(64, activation="relu")(x)
-outputs = layers.Dense(10)(x)
-
-model = models.Model(inputs=inputs, outputs=outputs)
-print(model.summary())
-
-
-(train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
-train_images, test_images = train_images / 255.0, test_images / 255.0
-
-
-model.compile(
-    optimizer="adam", loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=["accuracy"]
-)
-history = model.fit(train_images, train_labels, epochs=10, validation_data=(test_images, test_labels))
-
-plt.plot(history.history["accuracy"], label="accuracy")
-plt.plot(history.history["val_accuracy"], label="val_accuracy")
-plt.xlabel("Epoch")
-plt.ylabel("Accuracy")
-plt.ylim([0.5, 1])
-plt.legend(loc="lower right")
-
-# test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
-# print(test_acc)
+model = models.Sequential()
+model.add(layers.Conv2D(3, (3, 3), activation="relu", data_format="channels_last", input_shape=(None, None, 3)))
+model.add(layers.Dropout(0.2))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(3, (5, 5), activation="relu", padding="same"))
+model.add(layers.Dropout(0.2))
+model.add(layers.Conv2D(3, (3, 3), activation="relu", padding="same"))
+model.add(layers.Dropout(0.2))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Flatten())
+model.add(layers.Dense(500, activation="relu"))
+model.add(layers.Dropout(0.2))
+model.add(layers.Dense(500, activation="relu"))
+model.add(layers.Dropout(0.2))
+model.add(layers.Dense(500, activation="sigmoid"))
+model.add(layers.Dropout(0.2))
+model.summary()
