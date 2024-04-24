7b3ab65c52c3d5ee16cd3035fd07add8485c45f2
Michal Trela <mmichal.trela@gmail.com>
2024-04-24 22:20:50 +0200

    Changes to reduce dataset script

--- thermal_detection/reduce_dataset.py
+++ thermal_detection/reduce_dataset.py
@@ -3,10 +3,21 @@ import json
 import random
 from pathlib import Path
 from collections import defaultdict
+from typing import Dict, List
 
 
-def process_index_file(file_path: Path, number_of_frames_to_keep: int):
-    list_of_ids = defaultdict(list)
+def are_disjoint(list1, list2):
+    set1 = set(list1)
+    set2 = set(list2)
+    return set1.isdisjoint(set2)
+
+
+def process_index_file(
+    file_path: Path,
+    number_of_frames_with_class: int,
+    number_of_frames_without_class: int,
+):
+    object_class_by_frame_id: Dict[str, List[str]] = defaultdict(list)
 
     with file_path.open("r") as file:
         data = json.load(file)
@@ -15,31 +26,30 @@ def process_index_file(file_path: Path, number_of_frames_to_keep: int):
     for frame in data["frames"]:
         for annotation in frame["annotations"]:
             for label in annotation["labels"]:
-                list_of_ids[frame["datasetFrameId"]].append(label)
+                object_class_by_frame_id[frame["datasetFrameId"]].append(label)
 
-    classes_to_keep = ["person", "car", "bike"]
-    filtered_dict = {}
+    classes_to_keep = ["person"]
+    frames_containing_class: List[str] = []
+    frames_not_containing_class: List[str] = []
 
-    for key, values in list_of_ids.items():
-        if all(val in classes_to_keep for val in values):
-            filtered_dict[key] = values
+    for frame_id, class_list in object_class_by_frame_id.items():
+        if are_disjoint(class_list, classes_to_keep):
+            frames_not_containing_class.append(frame_id)
+        else:
+            frames_containing_class.append(frame_id)
 
-    list_of_ids.clear()
-    list_of_ids.update(filtered_dict)
-
-    for key, value in list_of_ids.items():
-        print(f"{key}: {value}")
-
-    keys_to_save = random.sample(filtered_dict.keys(), number_of_frames_to_keep)
+    frames_to_save = random.sample(frames_containing_class, number_of_frames_with_class) + random.sample(
+        frames_not_containing_class, number_of_frames_without_class
+    )
 
     for frame in data["frames"][:]:
-        if frame["datasetFrameId"] not in keys_to_save:
+        if frame["datasetFrameId"] not in frames_to_save:
             data["frames"].remove(frame)
 
     with file_path.open("w") as file:
         json.dump(data, file, indent=4)
 
-    return keys_to_save
+    return frames_to_save
 
 
 import os
@@ -62,19 +72,30 @@ app = typer.Typer(add_completion=False)
 def main(
     json_path: Path = typer.Argument(
         None,
-        help="Path to the json",
+        help="Path to json",
     ),
     dir_path: str = typer.Argument(
         None,
-        help="Path to the json",
+        help="Path to data",
     ),
-    number_of_frames_to_keep: int = typer.Argument(
+    number_of_frames_to_keep_class: int = typer.Argument(
         None,
-        help="Path to the json",
+        help="Number of frames to keep class",
+    ),
+    number_of_frames_to_keep_no_class: int = typer.Argument(
+        None,
+        help="Number of frames to keep no class",
     ),
 ):
 
-    delete_files(dir_path, process_index_file(json_path, number_of_frames_to_keep))
+    delete_files(
+        dir_path,
+        process_index_file(
+            json_path,
+            number_of_frames_to_keep_class,
+            number_of_frames_to_keep_no_class,
+        ),
+    )
 
 
 if __name__ == "__main__":
